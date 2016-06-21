ef0dadedb1d682c99e00670d66bdfaaf9c8fd1c5
dmarjanovic94 <dmarjanovic94@gmail.com>
2016-06-21 19:59:32 +0200

    Initial app commit.

---
+++ source/app.py
@@ -0,0 +1,16 @@
+import sys
+from PySide import QtGui
+
+class App(QtGui.QApplication):
+    def __init__(self, sys_argv):
+        super(App, self).__init__(sys_argv)
+        """
+        self.model = Model()
+        self.main_ctrl = MainController(self.model)
+        self.main_view = MainView(self.model, self.main_ctrl)
+        self.main_view.show()*
+        """
+
+if __name__ == '__main__':
+    app = App(sys.argv)
+    sys.exit(app.exec_())
